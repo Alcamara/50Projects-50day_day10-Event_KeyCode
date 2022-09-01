9520a8e7934b058bcfa726ff8169394a1fcf50c2
Al Camara <Alhousseinec@gmail.com>
2022-09-01 15:04:22 -0500

    added key event

--- client.js
+++ client.js
@@ -2,4 +2,35 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('test');
+    $('body').on('keypress',pressEvent)
+}
+
+function pressEvent(e) {
+    
+    $('.insert').empty()
+
+    const key = e.key;
+    const code = e.code;
+    const charCode = e.keyCode;
+    
+    $('#press').addClass('none');
+
+    $('.insert')
+        .append(`
+        <div class="key">
+        ${key} 
+        <small>event.key</small>
+     </div>
+
+     <div class="key">
+     ${charCode} 
+     <small>ev
+         <small>event.KeyCode</small>
+     </div>
+     <div class="key">
+        ${code}
+         <small>event.code</small>
+     </div>
+        `)
+       
 }
